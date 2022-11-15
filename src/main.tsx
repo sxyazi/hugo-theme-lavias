9a3334fc80ded06d6a59f5fcb96cf6d3c6995110
sxyazi <sxyazi@gmail.com>
2022-11-15 21:25:21 +0800

    feat: build the color system for `markdown-css`

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {render} from 'preact'
 import {App} from './App'
 import './main.css'
+import '@chisato/markdown-css/dist/colors.css'
 import '@chisato/markdown-css/dist/styles.css'
 import 'nprogress/nprogress.css'
 import {BrowserRouter} from 'react-router-dom'
